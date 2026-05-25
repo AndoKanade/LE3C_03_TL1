694aa69fecce0a41bd646a3b377178d0a23f7169
Kanade Andou <k024g0073@g.neec.ac.jp>
2026-05-25 12:27:00 +0900

    Update level_editor.py
    01_05完了

--- level_editor.py
+++ level_editor.py
@@ -18,6 +18,54 @@ bl_info = {
 def draw_menu_manual(self, context):
     self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
 
+# --- オペレータクラス：頂点を伸ばす ---
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました")
+        return {'FINISHED'}
+
+# --- オペレータクラス：ICO球生成 ---
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # メニューを実行したときに呼ばれる関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+        return {'FINISHED'}
+
+# --- メニュークラス ---
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    # メニュー内の描画設定
+    def draw(self, context):
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label) # 変更：メニューへの登録を追加
+
+    # サブメニューの追加処理
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# 登録対象のクラス一覧
+# 変更：Blenderに登録するクラスリストへの登録を追加
+classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
+    TOPBAR_MT_my_menu,
+)
+
 # アドオン有効化時の処理
 def register():
     # クラスの登録
@@ -41,23 +89,6 @@ def unregister():
         except:
             pass
     print("レベルエディタが無効化されました。")
-    
-# トップバーの拡張メニュー定義クラス
-class TOPBAR_MT_my_menu(bpy.types.Menu):
-    bl_idname = "TOPBAR_MT_my_menu"
-    bl_label = "MyMenu"
-    bl_description = "拡張メニュー by " + bl_info["author"]
-
-    # メニュー内の描画設定
-    def draw(self, context):
-        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
-
-    # サブメニューの追加処理
-    def submenu(self, context):
-        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
-
-# 登録対象のクラス一覧
-classes = (TOPBAR_MT_my_menu,)
 
 # スクリプト直接実行時の処理
 if __name__ == "__main__":
